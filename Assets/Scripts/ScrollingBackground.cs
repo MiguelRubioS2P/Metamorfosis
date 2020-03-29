f33e87b472c362efd9d7975c4c2c2791b8cf1588
fabiocardoso <fabiocardoso@paucasesnovescifp.cat>
2020-03-29 15:09:23 +0200

    Commit de la primera idea de menu principal

---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour {
+
+    public float bgSpeed;
+    public Renderer bgRend;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        bgRend.material.mainTextureOffset += new Vector2(bgSpeed * Time.deltaTime, 0f);
+    }
+}
