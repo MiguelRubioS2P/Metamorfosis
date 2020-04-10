acb94bebdfd62125e7743676b4af51da8a6abe1d
fabiocardoso <fabiocardoso@paucasesnovescifp.cat>
2020-04-10 12:40:06 +0200

    Moviento objecto 'Rodillos Pinchos' pendiente de verificacion

---
+++ Assets/Scripts/Objectos/Daño/Sierra_Movimiento.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sierra_Movimiento : MonoBehaviour
+{
+    public float fuerzaRotacion; // Variable para aplicar la fuerza de rotacion del objecto
+
+    void Update()
+    {
+        // Cada frame se rota el objecto por la fuerza de rotacion
+        transform.Rotate(0f, 0f, 1f * fuerzaRotacion);
+    }
+}
