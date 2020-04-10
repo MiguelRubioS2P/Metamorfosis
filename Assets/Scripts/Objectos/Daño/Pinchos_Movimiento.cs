1bcda6a8bbd8f929dea692f341949730626ece60
fabiocardoso <fabiocardoso@paucasesnovescifp.cat>
2020-04-10 12:23:48 +0200

    Movimento objecto 'Pincho Movimiento'

---
+++ Assets/Scripts/Objectos/Daño/Pinchos_Movimiento.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Pinchos_Movimiento : MonoBehaviour
+{
+    
+    public float fuerzaMovimiento; // Variable de fuerza de movimiento
+    private Vector2 posicionInicial; // Variable donde guardamos la posicion inicial
+
+    void Awake()
+    {
+        //Al inicar cogemos su posicion para poder volver a ella, despues iniciamos la coroutina.
+
+        posicionInicial = new Vector2(transform.position.x, transform.position.y);
+        StartCoroutine(Bajar());
+    }
+
+    IEnumerator Bajar()
+    {
+        //Esperamos 3s despues cambiamos su posicion hacia abajo dependiendo de la fuerza, 
+        //volvemos a esperar 3s y volvemos a la posicion inicial y volvemos a ejecutar.
+
+        yield return new WaitForSeconds(3);
+        transform.position = new Vector2(transform.position.x, transform.position.y * fuerzaMovimiento);
+        yield return new WaitForSeconds(3);
+        transform.position = posicionInicial;
+        StartCoroutine(Bajar());
+
+    }
+}
