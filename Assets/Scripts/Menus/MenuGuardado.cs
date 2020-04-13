a175b88d89688a04c47db3f7faf8a6e243eb36f9
fabiocardoso <fabiocardoso@paucasesnovescifp.cat>
2020-04-13 11:31:17 +0200

    Añadido menu guardado, retoques en iconos y funcionalida menu principal + menu guardado

---
+++ Assets/Scripts/Menus/MenuGuardado.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MenuGuardado : MonoBehaviour
+{
+    public Button slot1, slot2, slot3;
+   
+    public void OnClick()
+    {
+        // Guardamos en una variable el gameobject selecionado para saber por el 
+        // nombre que boton se ha seleccionado
+        string boton = EventSystem.current.currentSelectedGameObject.name;
+        if (boton == ("Slot 1 Boton"))
+        {
+            slot1.image.color = Color.green;
+            slot2.image.color = Color.red;
+            slot3.image.color = Color.red;
+        } else if (boton == ("Slot 2 Boton"))
+        {
+            slot2.image.color = Color.green;
+            slot1.image.color = Color.red;
+            slot3.image.color = Color.red;
+        }
+        else if (boton == ("Slot 3 Boton"))
+        {
+            slot3.image.color = Color.green;
+            slot2.image.color = Color.red;
+            slot1.image.color = Color.red;
+        }  else if (boton == ("Cancelar Boton"))
+        {
+            slot1.image.color = Color.red;
+            slot2.image.color = Color.red;
+            slot3.image.color = Color.red;
+        }
+    }
+
+    public void OnVolverMenu()
+    {
+        // Volver al menu
+        SceneManager.LoadScene("Menu Principal");
+    }
+
+    public void OnIrMenuNiveles()
+    {
+        // Aqui iria saber si ya tiene partida empezada o no
+        SceneManager.LoadScene("Menu Niveles");
+    }
+}
---
+++ Assets/Scripts/Menus/MenuPrincipal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    public void OnClick()
+    {
+        string boton = EventSystem.current.currentSelectedGameObject.name;
+        if (boton == "Play Boton")
+        {
+            SceneManager.LoadScene("Menu Guardado");
+        } 
+        else if (boton == "Opciones Boton")
+        {
+            SceneManager.LoadScene("Menu Opciones");
+        } 
+        else if (boton == "Salir Boton")
+        {
+            #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+            #else
+                Application.Quit();
+            #endif
+        }
+    }
+}
