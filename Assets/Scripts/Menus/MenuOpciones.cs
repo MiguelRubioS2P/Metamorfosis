e10326806255e8355b2181f1470628216fa88ae8
fabiocardoso <fabiocardoso@paucasesnovescifp.cat>
2020-04-17 14:27:13 +0200

    Funcionalida pre-alpha menu opciones

---
+++ Assets/Scripts/Menus/MenuOpciones.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuOpciones : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public Toggle pantallaCompleta, fps, wasd, arrows;
+
+    public void ControlVoluemnGeneral(float sliderValue)
+    {
+        mixer.SetFloat("Volumen", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void MenuPrincipal()
+    {
+        SceneManager.LoadScene("Menu Principal");
+    }
+
+    public void CambiarEstadoFPS()
+    {
+        if(fps.isOn)
+        {
+            GameObject.FindObjectOfType<OptionsManager>().GetComponent<OptionsManager>().fpsText.gameObject.SetActive(true);
+        }
+        else
+        {
+            GameObject.FindObjectOfType<OptionsManager>().GetComponent<OptionsManager>().fpsText.gameObject.SetActive(false);
+        }
+        
+    }
+}
