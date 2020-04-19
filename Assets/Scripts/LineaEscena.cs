5ff4a9f9a8f0b2b0ffa1e317fcb34070ba6eb939
Miguel Rubio <miguelrubio@paucasesnovescifp.cat>
2020-04-19 19:08:46 +0200

    Plataformas en movimiento, mejora de la rampa de la muerte

---
+++ Assets/Scripts/LineaEscena.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineaEscena : MonoBehaviour
+{
+
+    public Transform from;
+    public Transform to;
+
+
+    private void OnDrawGizmosSelected()
+    {
+        if(from != null && to != null)
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawLine(from.position,to.position);
+            Gizmos.DrawSphere(from.position, 0.15f);
+            Gizmos.DrawSphere(to.position, 0.15f);
+        }
+    }
+
+}
