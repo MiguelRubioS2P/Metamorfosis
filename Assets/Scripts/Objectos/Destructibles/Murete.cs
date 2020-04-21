bad4ca821bdc73b90dcd426ddddd23933e6e1ef3
fabiocardoso <fabiocardoso@paucasesnovescifp.cat>
2020-04-21 10:17:22 +0200

    Avances nivel 3

---
+++ Assets/Scripts/Objectos/Destructibles/Murete.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Murete : MonoBehaviour
+{
+    private int vida;
+
+    private void Awake()
+    {
+        vida = 3;
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Rango Ataque")
+        {
+            vida--;
+            if (vida == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
