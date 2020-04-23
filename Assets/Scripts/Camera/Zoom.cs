f706cdde074c4573c97c7d14db1ac744a81a8f8b
Miguel Rubio <miguelrubio@paucasesnovescifp.cat>
2020-04-23 20:13:47 +0200

    rediseño nivel 2

--- Assets/Scripts/Camera/Zoom.cs
+++ Assets/Scripts/Camera/Zoom.cs
@@ -7,18 +7,6 @@ public class Zoom : MonoBehaviour
     public Camera camara;
     public float zoom;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.transform.tag == "Player")
