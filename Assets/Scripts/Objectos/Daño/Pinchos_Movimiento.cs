00001e4ea947871de1a8879a45445121a3696335
fabiocardoso <fabiocardoso@paucasesnovescifp.cat>
2020-04-30 13:05:24 +0200

    Movimiento ams realista bloque de pinchos

--- Assets/Scripts/Objectos/Daño/Pinchos_Movimiento.cs
+++ Assets/Scripts/Objectos/Daño/Pinchos_Movimiento.cs
@@ -3,29 +3,41 @@ using UnityEngine;
 
 public class Pinchos_Movimiento : MonoBehaviour
 {
-    public float fuerzaMovimiento; // Variable de fuerza de movimiento
     private Vector2 posicionInicial; // Variable donde guardamos la posicion inicial
-    
+    private float posicionFinal;
+    private Rigidbody2D rigidbody2d;
+
+
 
 
     void Awake()
     {
         //Al inicar cogemos su posicion para poder volver a ella, despues iniciamos la coroutina.
         posicionInicial = new Vector2(transform.position.x, transform.position.y);
+        rigidbody2d = GetComponent<Rigidbody2D>();
         StartCoroutine(Bajar());
     }
-
-
-   
 
     IEnumerator Bajar()
     {
         //Esperamos 3s despues cambiamos su posicion hacia abajo dependiendo de la fuerza, 
         //volvemos a esperar 3s y volvemos a la posicion inicial y volvemos a ejecutar.
         yield return new WaitForSeconds(3);
-        transform.position = new Vector2(transform.position.x, transform.position.y * fuerzaMovimiento);
+        rigidbody2d.isKinematic = false;
+
         yield return new WaitForSeconds(3);
-        transform.position = posicionInicial;
+        rigidbody2d.isKinematic = true;
+
+        var posicionActual = transform.position;
+        var tiempo = 0f;
+        var segundos = 1f;
+        while (tiempo < 1)
+        {
+            tiempo += Time.deltaTime / segundos;
+            transform.position = Vector3.Lerp(posicionActual, posicionInicial, tiempo);
+            yield return null;
+        }
+
         StartCoroutine(Bajar());
     }
 }
