c2f4cbc55160e48ae1ef0aa3df3f134e3ed8b489
MiguelRubioS2P <miguelrubio@paucasesnovescifp.cat>
2020-05-09 10:52:24 +0200

    Funcionalidad nueva en el nivel 4

---
+++ Assets/Scripts/Terreno/TP.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TP : MonoBehaviour
+{
+
+    public GameObject posicionFinal;
+    public GameObject player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.transform.tag == "Player")
+        {
+            Debug.Log("Hola, estoy dentro");
+            player.transform.position = posicionFinal.transform.position;
+            Destroy(gameObject);
+        }
+    }
+}
