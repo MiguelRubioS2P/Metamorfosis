c6eebe1023796f0e33cf9c12e5b48ab279d6ef0c
MiguelRubioS2P <miguelrubio@paucasesnovescifp.cat>
2020-05-13 17:06:55 +0200

    Doble click en el menú de guardado para acceder a la partida seleccionada

---
+++ Assets/Scripts/Menus/DoubleClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DoubleClick : MonoBehaviour, IPointerClickHandler
+{
+    private MenuGuardado menuGuardadoScript;
+
+    private void Awake()
+    {
+        menuGuardadoScript = FindObjectOfType<MenuGuardado>();
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.clickCount == 2)
+        {
+            Debug.Log("double click");
+            menuGuardadoScript.OnIrMenuNiveles();
+        }
+    }
+}
